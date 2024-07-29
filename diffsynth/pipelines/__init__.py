3981b8084fac8e803bcb701bbd6f37d5b0f05177
Artiprocher <wangye87v5@hotmail.com>
2024-07-29 10:22:47 +0800

    redirect Kolors

--- diffsynth/pipelines/__init__.py
+++ diffsynth/pipelines/__init__.py
@@ -6,3 +6,4 @@ from .sd3_image import SD3ImagePipeline
 from .hunyuan_image import HunyuanDiTImagePipeline
 from .svd_video import SVDVideoPipeline
 from .pipeline_runner import SDVideoPipelineRunner
+KolorsImagePipeline = SDXLImagePipeline
