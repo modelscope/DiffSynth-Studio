66f1ff43e974569bf661f254114cd234cd95d0ca
Artiprocher <wangye87v5@hotmail.com>
2024-08-22 10:35:58 +0800

    update examples

--- examples/image_synthesis/flux_text_to_image.py
+++ examples/image_synthesis/flux_text_to_image.py
@@ -12,14 +12,14 @@ model_manager.load_models([
 ])
 pipe = FluxImagePipeline.from_model_manager(model_manager)
 
-prompt = "A captivating fantasy magic woman portrait set in the deep sea. The woman, with blue spaghetti strap silk dress, swims in the sea. Her flowing silver hair shimmers with every color of the rainbow and cascades down, merging with the floating flora around her. Smooth, delicate and fair skin."
-negative_prompt = "worst quality, low quality, monochrome, zombie, interlocked fingers, Aissist, dim, fuzzy, depth of Field, nsfw,"
+prompt = "CG. Full body. A captivating fantasy magic woman portrait in the deep sea. The woman, with blue spaghetti strap silk dress, swims in the sea. Her flowing silver hair shimmers with every color of the rainbow and cascades down, merging with the floating flora around her. Smooth, delicate and fair skin."
+negative_prompt = "dark, worst quality, low quality, monochrome, zombie, interlocked fingers, Aissist, dim, fuzzy, depth of Field, nsfw,"
 
 # Disable classifier-free guidance (consistent with the original implementation of FLUX.1)
 torch.manual_seed(6)
 image = pipe(
     prompt=prompt,
-    num_inference_steps=30,
+    num_inference_steps=30, embedded_guidance=3.5
 )
 image.save("image_1024.jpg")
 
@@ -27,7 +27,7 @@ image.save("image_1024.jpg")
 torch.manual_seed(6)
 image = pipe(
     prompt=prompt, negative_prompt=negative_prompt,
-    num_inference_steps=30, cfg_scale=2.0
+    num_inference_steps=30, cfg_scale=2.0, embedded_guidance=3.5
 )
 image.save("image_1024_cfg.jpg")
 
@@ -35,7 +35,7 @@ image.save("image_1024_cfg.jpg")
 torch.manual_seed(7)
 image = pipe(
     prompt=prompt,
-    num_inference_steps=30,
+    num_inference_steps=30, embedded_guidance=3.5,
     input_image=image.resize((2048, 2048)), height=2048, width=2048, denoising_strength=0.6, tiled=True
 )
 image.save("image_2048_highres.jpg")
