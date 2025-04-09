cd8884c9efedba68ddf485ac29c28e3d93424b83
Zhongjie Duan <35051019+Artiprocher@users.noreply.github.com>
2025-04-09 15:27:36 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name="diffsynth",
-    version="1.1.4",
+    version="1.1.5",
     description="Enjoy the magic of Diffusion models!",
     author="Artiprocher",
     packages=find_packages(),
