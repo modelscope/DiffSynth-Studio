9e683bfe25886b5de5eaefd865ceae61796cd14e
Artiprocher <wangye87v5@hotmail.com>
2025-07-28 18:30:04 +0800

    fix typo

--- examples/wanvideo/model_training/validate_lora/Wan2.2-TI2V-5B.py
+++ examples/wanvideo/model_training/validate_lora/Wan2.2-TI2V-5B.py
@@ -24,6 +24,6 @@ video = pipe(
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
     input_image=input_image,
     num_frames=49,
-    seed=1, tiled=False,
+    seed=1, tiled=True,
 )
 save_video(video, "video_Wan2.2-TI2V-5B.mp4", fps=15, quality=5)
