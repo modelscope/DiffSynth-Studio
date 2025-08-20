7e348083ae6b39934cc60973a1db9b7c9020482a
mi804 <1576993271@qq.com>
2025-08-20 12:42:11 +0800

    minor fix

--- examples/qwen_image/model_inference/Qwen-Image-Edit.py
+++ examples/qwen_image/model_inference/Qwen-Image-Edit.py
@@ -18,9 +18,9 @@ input_image.save("image1.jpg")
 
 prompt = "将裙子改为粉色"
 # edit_image_auto_resize=True: auto resize input image to match the area of 1024*1024 with the original aspect ratio
-# edit_image_auto_resize=False: do not resize input image
 image = pipe(prompt, edit_image=input_image, seed=1, num_inference_steps=40, height=1328, width=1024, edit_image_auto_resize=True)
 image.save(f"image2.jpg")
 
+# edit_image_auto_resize=False: do not resize input image
 image = pipe(prompt, edit_image=input_image, seed=1, num_inference_steps=40, height=1328, width=1024, edit_image_auto_resize=False)
 image.save(f"image3.jpg")
