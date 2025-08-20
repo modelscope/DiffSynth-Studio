d0467a7e8d40035ed097c2307c19f42905bb1bc4
mi804 <1576993271@qq.com>
2025-08-20 23:28:40 +0800

    fix controlnet annotator

--- examples/qwen_image/model_inference/Qwen-Image-Context-Control.py
+++ examples/qwen_image/model_inference/Qwen-Image-Context-Control.py
@@ -3,9 +3,9 @@ import torch
 from modelscope import dataset_snapshot_download, snapshot_download
 from diffsynth.pipelines.qwen_image import QwenImagePipeline, ModelConfig
 from diffsynth.controlnets.processors import Annotator
-from diffsynth import download_models
 
-download_models(["Annotators:Depth"])
+allow_file_pattern = ["sk_model.pth", "sk_model2.pth", "dpt_hybrid-midas-501f0c75.pt", "ControlNetHED.pth", "body_pose_model.pth", "hand_pose_model.pth", "facenet.pth", "scannet.pt"]
+snapshot_download("lllyasviel/Annotators", local_dir="models/Annotators", allow_file_pattern=allow_file_pattern)
 
 pipe = QwenImagePipeline.from_pretrained(
     torch_dtype=torch.bfloat16,
