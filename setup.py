1313f4dd63d3e46d7dac532083f4886118641262
Artiprocher <wangye87v5@hotmail.com>
2025-11-18 10:29:07 +0800

    support downloading resource

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ else:
 
 setup(
     name="diffsynth",
-    version="1.1.8",
+    version="1.1.9",
     description="Enjoy the magic of Diffusion models!",
     author="Artiprocher",
     packages=find_packages(),
