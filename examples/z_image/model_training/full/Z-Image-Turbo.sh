6fbd9e94ec954dbde0f4c890b232e61ded314df6
Artiprocher <wangye87v5@hotmail.com>
2025-12-04 13:53:13 +0800

    update examples

--- examples/z_image/model_training/full/Z-Image-Turbo.sh
+++ examples/z_image/model_training/full/Z-Image-Turbo.sh
@@ -1,8 +1,9 @@
-accelerate launch examples/z_image/model_training/train.py \
+# This example is tested on 8*A100
+accelerate launch --config_file examples/z_image/model_training/full/accelerate_config.yaml examples/z_image/model_training/train.py \
   --dataset_base_path data/example_image_dataset \
   --dataset_metadata_path data/example_image_dataset/metadata.csv \
   --max_pixels 1048576 \
-  --dataset_repeat 50 \
+  --dataset_repeat 400 \
   --model_id_with_origin_paths "Tongyi-MAI/Z-Image-Turbo:transformer/*.safetensors,Tongyi-MAI/Z-Image-Turbo:text_encoder/*.safetensors,Tongyi-MAI/Z-Image-Turbo:vae/diffusion_pytorch_model.safetensors" \
   --learning_rate 1e-5 \
   --num_epochs 2 \
