edce95313849c94762ee777033a27654b435e6db
ahmeddawy <ahmed94dawy@gmail.com>
2026-01-15 10:08:30 +0000

    Resolve stash conflict in Wan2.1-VACE-1.3B lora train script

--- examples/wanvideo/model_training/lora/Wan2.1-VACE-1.3B.sh
+++ examples/wanvideo/model_training/lora/Wan2.1-VACE-1.3B.sh
@@ -1,15 +1,15 @@
 accelerate launch examples/wanvideo/model_training/train.py \
   --dataset_base_path /mnt/bucket/dawy/video_generation/two_stage_dataset \
-  --dataset_metadata_path /mnt/bucket/dawy/video_generation/two_stage_dataset/metadata_vanilla_stage1.csv \
+  --dataset_metadata_path /mnt/bucket/dawy/video_generation/two_stage_dataset/metadata_vanilla_stage1_train.csv \
   --data_file_keys "video,vace_video,vace_reference_image,vace_video_mask" \
   --height 480 \
   --width 832 \
-  --dataset_repeat 1 \
+  --dataset_repeat 2 \
   --model_id_with_origin_paths "Wan-AI/Wan2.1-VACE-1.3B:diffusion_pytorch_model*.safetensors,Wan-AI/Wan2.1-VACE-1.3B:models_t5_umt5-xxl-enc-bf16.pth,Wan-AI/Wan2.1-VACE-1.3B:Wan2.1_VAE.pth" \
   --learning_rate 1e-4 \
-  --batch_size 2 \
+  --batch_size 5 \
   --gradient_accumulation_steps 4 \
-  --num_epochs 5 \
+  --num_epochs 25 \
   --remove_prefix_in_ckpt "pipe.vace." \
   --output_path "./models/train/Wan2.1-VACE-1.3B_lora" \
   --lora_base_model "vace" \
@@ -18,7 +18,7 @@ accelerate launch examples/wanvideo/model_training/train.py \
   --extra_inputs "video,vace_video,vace_reference_image,vace_video_mask" \
   --use_gradient_checkpointing_offload \
   --val_dataset_base_path /mnt/bucket/dawy/video_generation/two_stage_dataset \
-  --val_dataset_metadata_path /mnt/bucket/dawy/video_generation/two_stage_dataset/metadata_vanilla_stage1_val.csv \
+  --val_dataset_metadata_path /mnt/bucket/dawy/video_generation/two_stage_dataset/metadata_vanilla_stage1_eval.csv \
   --val_data_file_keys "video,vace_video,vace_reference_image,vace_video_mask" \
   --val_batch_size 2 \
   --eval_every_n_epochs 1 \
