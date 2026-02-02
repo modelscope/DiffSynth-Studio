c778a3e76272de6be7fbe8193e565806925449da
Manthan Solanki <manthansolanki1999@gmail.com>
2026-02-02 12:33:30 +0100

    params updated

--- examples/wanvideo/model_training/validate_lora/Wan2.1-VACE-1.3B.py
+++ examples/wanvideo/model_training/validate_lora/Wan2.1-VACE-1.3B.py
@@ -13,25 +13,20 @@ pipe = WanVideoPipeline.from_pretrained(
         ModelConfig(path="models/Wan-AI/Wan2.1-VACE-1.3B/Wan2.1_VAE.pth"),
     ],
 )
-pipe.load_lora(pipe.vace, "models/train/Wan2.1-VACE-1.3B_lora/step-1100.safetensors", alpha=0.2)
+pipe.load_lora(pipe.vace, "models/train/Wan2.1-VACE-1.3B_lora/step-1100.safetensors", alpha=0.1)
 
 video = VideoData("data_infer/processed/pose/dance-4_1_pose.mp4", height=480, width=832)
 video = [video[i] for i in range(65)]
-# ref_img = VideoData("data_infer/ref_img.jpg", height=480, width=832)[0]
-# ref_img_1 = VideoData("data_infer/ref_img_1.jpg", height=480, width=832)[0]
-# ref_img_2 = VideoData("data_infer/ref_img_2.jpg", height=480, width=832)[0]
-# ref_img_3 = VideoData("data_infer/ref_img_3.jpg", height=480, width=832)[0]
-# ref_img_4 = VideoData("data_infer/ref_img_4.jpg", height=480, width=832)[0]
-
-other_1 = VideoData("data_infer/other_1.jpg", height=480, width=832)[0]
+ref_img = VideoData("data_infer/ref_img.jpg", height=480, width=832)[0]
+ref_img_1 = VideoData("data_infer/ref_img_1.jpg", height=480, width=832)[0]
+ref_img_2 = VideoData("data_infer/ref_img_2.jpg", height=480, width=832)[0]
+ref_img_3 = VideoData("data_infer/ref_img_3.jpg", height=480, width=832)[0]
+ref_img_4 = VideoData("data_infer/ref_img_4.jpg", height=480, width=832)[0]
 
 video = pipe(
-    prompt="Person is dancing by following the video pose exactly. Dance is natural and smooth. Maintain the exact facial features, hair, clothing, and background from the reference image. Keep the background consistent with the reference image.",
+    prompt="Person is dancing by following the video pose exactly. Dance is natural and smooth. Maintain the exact facial features, hair, clothing, and background from the reference images. Keep the background consistent with the reference images. He is wearing black t-shirt, dark gray jeans, and white sneakers shoes. The background contain white wall, and wooden flooring with horizontal line as shown in the reference image. Keep the face from reference image. Hands has 5 fingers, short hair, and slim body.",
     negative_prompt="色调艳丽，过曝，静态，细节模糊不清，字幕，风格，作品，画作，画面，静止，整体发灰，最差质量，低质量，JPEG压缩残留，丑陋的，残缺的，多余的手指，画得不好的手部，画得不好的脸部，畸形的，毁容的，形态畸形的肢体，手指融合，静止不动的画面，杂乱的背景，三条腿，背景人很多，倒着走",
-    vace_video=video, vace_reference_image=other_1, num_frames=65,
+    vace_video=video, vace_reference_image=[ref_img, ref_img_1, ref_img_2, ref_img_3, ref_img_4], num_frames=65,
     seed=1, tiled=True
 )
 save_video(video, "results/lora_dance.mp4", fps=15, quality=9)
-
-
-# prompt="Person is dancing by following the video pose exactly. Dance is natural and smooth. Maintain the exact facial features, hair, clothing, and background from the reference images. Keep the background consistent with the reference images. He is wearing black t-shirt, dark gray jeans, and white sneakers shoes. The background contain white wall, and wooden flooring with horizontal line as shown in the reference image. Keep the face from reference image. Hands has 5 fingers, short hair, and slim body.",
