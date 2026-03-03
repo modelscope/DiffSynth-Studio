add6f88324f19a1487089823d8d5345698def298
Artiprocher <wangye87v5@hotmail.com>
2026-03-03 15:33:42 +0800

    bugfix

--- diffsynth/configs/__init__.py
+++ diffsynth/configs/__init__.py
@@ -1,2 +1,2 @@
 from .model_configs import MODEL_CONFIGS
-from .vram_management_module_maps import VRAM_MANAGEMENT_MODULE_MAPS
+from .vram_management_module_maps import VRAM_MANAGEMENT_MODULE_MAPS, VERSION_CHECKER_MAPS
