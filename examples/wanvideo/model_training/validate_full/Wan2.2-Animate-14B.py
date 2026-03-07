42c6903b326a40159c7b618e25f097dea69e0fb7
zwplus <158684334@qq.com>
2026-03-08 02:05:21 +0800

    Use 77 frames in wan-animate examples

--- examples/wanvideo/model_training/validate_full/Wan2.2-Animate-14B.py
+++ examples/wanvideo/model_training/validate_full/Wan2.2-Animate-14B.py
@@ -1,4 +1,4 @@
-import torch
+﻿import torch
 from PIL import Image
 from diffsynth.utils.data import save_video, VideoData
 from diffsynth.core import load_state_dict
@@ -19,15 +19,16 @@ state_dict = load_state_dict("models/train/Wan2.2-Animate-14B_full/epoch-1.safet
 pipe.animate_adapter.load_state_dict(state_dict, strict=False)
 
 input_image = VideoData("data/example_video_dataset/animate/animate_output.mp4", height=480, width=832)[0]
-animate_pose_video = VideoData("data/examples/wan/animate/animate_pose_video.mp4", height=480, width=832).raw_data()[:81-4]
-animate_face_video = VideoData("data/examples/wan/animate/animate_face_video.mp4", height=512, width=512).raw_data()[:81-4]
+animate_pose_video = VideoData("data/examples/wan/animate/animate_pose_video.mp4", height=480, width=832).raw_data()[:77]
+animate_face_video = VideoData("data/examples/wan/animate/animate_face_video.mp4", height=512, width=512).raw_data()[:77]
 video = pipe(
     prompt="视频中的人在做动作",
     seed=0, tiled=True,
     input_image=input_image,
     animate_pose_video=animate_pose_video,
     animate_face_video=animate_face_video,
-    num_frames=81, height=480, width=832,
+    num_frames=77, height=480, width=832,
     num_inference_steps=20, cfg_scale=1,
 )
 save_video(video, "video_Wan2.2-Animate-14B.mp4", fps=15, quality=5)
+
