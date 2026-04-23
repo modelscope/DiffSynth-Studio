9453700a300cbfc8606f33372b9e15feb1f23fd9
mi804 <1576993271@qq.com>
2026-04-23 18:26:25 +0800

    sdxl modelcode

--- diffsynth/utils/state_dict_converters/stable_diffusion_xl_text_encoder.py
+++ diffsynth/utils/state_dict_converters/stable_diffusion_xl_text_encoder.py
@@ -1,7 +1,13 @@
+import torch
+
 def SDXLTextEncoder2StateDictConverter(state_dict):
     new_state_dict = {}
     for key in state_dict:
-        if key.startswith("text_model.") and "position_ids" not in key:
+        if key == "text_projection.weight":
+            val = state_dict[key]
+            new_state_dict["model.text_projection.weight"] = val.float() if val.dtype == torch.float16 else val
+        elif key.startswith("text_model.") and "position_ids" not in key:
             new_key = "model." + key
-            new_state_dict[new_key] = state_dict[key]
+            val = state_dict[key]
+            new_state_dict[new_key] = val.float() if val.dtype == torch.float16 else val
     return new_state_dict
