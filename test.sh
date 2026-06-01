29d8b793a2aedf2595ad8b7180748425b67c1261
yuze <StevenSun@sjtu.edu.cn>
2026-06-01 17:01:02 +0800

    try

--- test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=$(pwd):$PYTHONPATH
-
-NGPUS=${NGPUS:-1}
-
-srun --partition=medai_p --mpi=pmi2 --gres=gpu:${NGPUS} --quotatype=reserved \
-     -n1 --ntasks-per-node=1 --cpus-per-task=8 \
-     --job-name=lpips --kill-on-bad-exit=1 \
-     python examples/image_quality_metric/lpips.py
