0b043c53493b744e2eff9d846e47040e2ea1d3f9
yjy415 <2471352175@qq.com>
2026-07-27 11:29:28 +0800

    fix: Tc-LoRA Loader

--- diffsynth/utils/lora/flux_timestep.py
+++ diffsynth/utils/lora/flux_timestep.py
@@ -66,17 +66,6 @@ class FluxTimestepLoRALoader:
         self.device = device
         self.torch_dtype = torch_dtype
 
-    @staticmethod
-    def resolve_lora_file(lora_config):
-        lora_config.download_if_necessary()
-        paths = lora_config.path
-        lora_paths = [Path(path) for path in paths]
-        lora_files = []
-        for path in lora_paths:
-            if path.suffix == ".safetensors":
-                lora_files.append(path)
-        return lora_files[0]
-
     def load_adapter_scaling(self, lora_file):
         config_path = lora_file.parent / "adapter_config.json"
         with config_path.open("r", encoding="utf-8") as file:
@@ -150,10 +139,12 @@ class FluxTimestepLoRALoader:
 
         return model_fn_with_timestep_lora
 
-    def load(self, dit, lora_config, alpha=1):
-        lora_file = self.resolve_lora_file(lora_config)
-        state_dict = load_state_dict(str(lora_file), torch_dtype=self.torch_dtype, device=self.device)
-        scaling = self.load_adapter_scaling(lora_file) * alpha
+    def load(self, dit, lora_config, alpha=1.0):
+        lora_config.download_if_necessary()
+        paths = lora_config.path
+        lora_file = next(path for path in paths if path.endswith(".safetensors"))
+        state_dict = load_state_dict(lora_file, torch_dtype=self.torch_dtype, device=self.device)
+        scaling = self.load_adapter_scaling(Path(lora_file)) * alpha
 
         for block_id in range(len(dit.blocks)):
             source_prefix = f"transformer_blocks.{block_id}"
