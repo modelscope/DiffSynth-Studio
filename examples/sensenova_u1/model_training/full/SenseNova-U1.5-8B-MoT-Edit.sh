f5630ab072ab948721aff0a8b3bec2092711797a
mi804 <1576993271@qq.com>
2026-08-31 19:21:29 +0800

    add zero2

--- examples/sensenova_u1/model_training/full/SenseNova-U1.5-8B-MoT-Edit.sh
+++ examples/sensenova_u1/model_training/full/SenseNova-U1.5-8B-MoT-Edit.sh
@@ -1,6 +1,6 @@
 modelscope download --dataset DiffSynth-Studio/diffsynth_example_dataset --include "sensenova_u1/SenseNova-U1.5-8B-MoT-Edit/*" --local_dir ./data/diffsynth_example_dataset
 
-accelerate launch --config_file examples/sensenova_u1/model_training/full/accelerate_config_zero3.yaml examples/sensenova_u1/model_training/train.py \
+accelerate launch --config_file examples/sensenova_u1/model_training/full/accelerate_config_zero2.yaml examples/sensenova_u1/model_training/train.py \
   --dataset_base_path data/diffsynth_example_dataset/sensenova_u1/SenseNova-U1.5-8B-MoT-Edit \
   --dataset_metadata_path data/diffsynth_example_dataset/sensenova_u1/SenseNova-U1.5-8B-MoT-Edit/metadata.json \
   --data_file_keys "image,edit_image" \
